df6578551b1b182e66a530a6d884681dcd20c74e
jaishreeK <jaishreekulkarni@gmail.com>
2019-07-22 22:06:18 +1200

    updated tests

--- StringReassemblyTest/StringAssemblyTest.cs
+++ StringReassemblyTest/StringAssemblyTest.cs
@@ -109,16 +109,5 @@ namespace StringReassemblyTest
             strAssembly.AssembleStrings(stringSet);
             Assert.AreEqual("a quick brown fox. jumps over, the lazy dog.", stringSet[0]);
         }
-
-        [TestMethod]
-        public void TestAssemblyOverlapEx5()
-        {
-            var fragments = "m quaerat voluptatem.;pora incidunt ut labore et d;, consectetur, adipisci velit;olore magnam aliqua;idunt ut labore et dolore magn;uptatem.;i dolorem ipsum qu;iquam quaerat vol;psum quia dolor sit amet, consectetur, a;ia dolor sit amet, conse;squam est, qui do;Neque porro quisquam est, qu;aerat voluptatem.;m eius modi tem;Neque porro qui;, sed quia non numquam ei;lorem ipsum quia dolor sit amet;ctetur, adipisci velit, sed quia non numq;unt ut labore et dolore magnam aliquam qu;dipisci velit, sed quia non numqua;us modi tempora incid;Neque porro quisquam est, qui dolorem i;uam eius modi tem;pora inc;am al"
-                                .Split(';').Select(s => s.TrimStart()).ToList();
-            string result = strAssembly.AssembleStrings(fragments);
-            Debug.Print(result);
-            Assert.IsTrue(result == "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.");
-
-        }
     }
 }
